7e17e06214de8d35e873aa50dfa19b17f1efab28
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-07-23 22:54:30 -0700

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Mr. Chocolate', 5, 'F','black','angry',true);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Rowdy', 3, 'M','brown','friendly',true);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Tabitha', 2, 'M','white','funny',false);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Sergeant Brown', 7, 'M','grey','calm',true);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Melissa', 4, 'F','purple','smiley',true);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Grinch', 7, 'M','blue','nice',false);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES ('Wendy', 5, 'F','yellow','angry',true);
+INSERT INTO bears (name, age, sex,color,temperament,alive) VALUES (NULL, 6, 'F','black','funny',false);
